09cb95bf1c7ea4fd08ebe72028caaf74aa69dffc
Asmahan-Rashed <65909593+Asmahan-Rashed@users.noreply.github.com>
2022-01-09 15:40:28 +0200

    Update Code.asm

--- Code.asm
+++ Code.asm
@@ -1,91 +1,286 @@
-.386
-INCLUDE Irvine32.inc
-.MODELSMALL
 .DATA
 
+sum dw 0
+v dw 2 DUP(0)
+v0 dw 10325
+v1 dw 20123
+delta dw 0130h
+;k dw 4 DUP(0),0
+k0 dw 1
+k1 dw 2
+k2 dw 3
+k3 dw 4
 
-sum dd 0
-values dd 2 DUP(0)
-v0 dd ?
-v1 dd ?
-delta dd 9e3779b9h ;constant
-key dd 4 DUP(0)
-
-splitOut db 8 DUP(0), 0
-
+linefeed db 13, 10, "$"
 
 .CODE
+main PROC
+         MOV AX,@DATA
+         MOV DS,AX
+         
+         ;Print initial value in v0
+         mov ax,v0
+         CALL PRINT
+         
+         ;new line
+         mov ah, 09
+         mov dx, offset linefeed
+         int 21h
+         
+         ;Print initial value in v1
+         mov ax,v1
+         CALL PRINT
+         
+         ;new line
+         mov ah, 09
+         mov dx, offset linefeed
+         int 21h
+         
+         
+         ;Do encryption for (v0 & v1)
+         CALL encrypt
+         
+         ;new line
+         mov ah, 09
+         mov dx, offset linefeed
+         int 21h 
+         
+         ;Do encryption for (v0 & v1)
+         CALL decrypt
+         
+         ;new line
+         mov ah, 09
+         mov dx, offset linefeed
+         int 21h
+         
+         
+         ;interrupt to exit			
+	     MOV AH,4CH
+	     INT 21H
 
-encrypt procedure
+            
+          
+            
+            
+            
+            
+            
+main ENDP
+
+
+encrypt PROC
+    
     mov sum, 0
-    mov eax, [values]
-    mov v0, eax
-    mov eax, [values+4]
-    mov v1, eax
-    mov ecx, 32
+    
+    mov cx, 32
+
     encryptLoop:
-        mov eax, delta
-        add sum ,eax
-        mov eax, v1
-        shl eax, 4
-        add eax, [key] ; eax =(v1 << 4) + key[0])
-        mov ebx, v1
-        add ebx, sum ; ebx = (v1 + sum)
-        xor eax, ebx ; eax = (v1 << 4) + key[0]) ^ (v1 + sum)
-        mov ebx, v1
-        shr ebx, 5  ;ebx = (v1 >> 5)
-        add ebx, [key+4]    ;ebx =(v1 >> 5) + key[1]
-        xor eax, ebx  ; eax = ((v1 << 4) + key[0]) ^ (v1 + sum) ^ ((v1 >> 5) + key[1])
-        add v0, eax ;v0 += ((v1 << 4) + key[0]) ^ (v1 + sum) ^ ((v1 >> 5) + key[1])
-        mov eax, v0
-        shl eax, 4
-        add eax, [key+8] ; eax =(v0 << 4) + key[2])
-        mov ebx, v0
-        add ebx, sum ; ebx = (v0 + sum)
-        xor eax, ebx ; eax = (v0 << 4) + key[2]) ^ (v0 + sum)
-        mov ebx, v0
-        shr ebx, 5  ;ebx = (v0 >> 5)
-        add ebx, [key+12]    ;ebx =(v0 >> 5) + key[3]
-        xor eax, ebx  ; eax = ((v0 << 4) + key[2]) ^ (v0 + sum) ^ ((v0 >> 5) + key[3])
-        add v1, eax ;v1 += ((v0 << 4) + key[2]) ^ (v0 + sum) ^ ((v0 >> 5) + key[3])
+    
+        ;sum+=delta
+        mov ax, delta
+        add sum ,ax
+        
+        ;(v1<<4)+k0
+        mov ax, v1
+        shl ax, 4
+        add ax, k0
+        
+        ;v1+sum 
+        mov bx, v1
+        add bx, sum 
+        
+        xor ax, bx 
+        
+        ;(v1>>5)+k1
+        mov bx, v1
+        shr bx, 5  
+        add bx, k1 
+        
+        xor ax, bx  
+        
+        ;v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1)
+        add v0, ax 
+
+        ;(v0<<4)+k2
+        mov ax, v0
+        shl ax, 4
+        add ax, k2 
+        
+        ;v0+sum
+        mov bx, v0
+        add bx, sum 
+        
+        xor ax, bx
+        
+        ;(v0>>5) + k3
+        mov bx, v0
+        shr bx, 5  
+        add bx, k3
+        
+        xor ax, bx  
+        ;v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3)
+        add v1, ax 
+
     loop encryptLoop
-    mov eax, v0
-    mov [values] , eax
-    mov eax, v1
-    mov [values+4] , eax
+
+    ;values[0] = v0
+    mov ax, v0
+    CALL PRINT
+    mov [v] , ax
+    
+    ;new line
+    mov ah, 09
+    mov dx, offset linefeed
+    int 21h
+    
+
+    ;values[1] = v1
+    mov ax, v1
+    CALL PRINT
+    mov [v+2] , ax
+    
+    ;new line
+    mov ah, 09
+    mov dx, offset linefeed
+    int 21h
 
     ret
 
 encrypt ENDP
 
-split PROC
-    mov eax, values[0]
-    lea ebx, splitOut ; ebx = offset splitOut
-    mov ecx, 2 ;counter of outerloop
-    splitOuterLoop:
+
+decrypt PROC
+    
+    ;sum = delta<<5
+    mov bx, delta
+    shl bx, 5
+    AND bx, 0FFFFH 
+    mov sum, bx
+
+    ;loop 32 times
+    mov cx, 32
+
+    decryptLoop:
+
+        ;(v0<<4)+k2
+        mov ax, v0
+        shl ax, 4
+        add ax, k2 
+    
+        ;(v0 + sum)
+        mov bx, v0
+        add bx, sum
+
+        xor ax, bx 
+        ;(v0 >> 5) + k3
+        mov bx, v0
+        shr bx, 5  
+        add bx, k3    
+
+        xor ax,bx  
+
+        sub v1, ax 
         
-        mov edx, ecx ;storing the counter of outerloop in edx
+        ;(v1<<4)+k0
+        mov ax, v1
+        shl ax, 4
+        add ax, k0
+    
+        ;(v1 + sum)
+        mov bx, v1
+        add bx, sum
 
-        mov ecx, 2 ;setting counter of inner loop
-  
-        splitInnerLoop:
+        xor ax, bx 
 
-                mov [ebx], al
-                inc ebx
+        ;(v1 >> 5) + k1
+        mov bx, v1
+        shr bx, 5  
+        add bx, k1   
+        
+        xor ax, bx 
 
-                mov [ebx], ah
-                inc ebx
+        sub v0, ax 
 
-                shr eax, 16
 
-        LOOP splitInnerLoop
+        ;sum -= delta;
+        mov ax, delta
+        sub sum ,ax
 
-        ;splitting second 4 chars
-        mov eax, values[4]
+    loop decryptLoop
 
-        mov ecx, edx ;putting the counter of outer loop back in ecx
-    LOOP splitOuterLoop
+    ;values[0] = v0
+    mov ax, v0
+    CALL PRINT
+    mov [v] , ax
+    
+    ;new line
+    mov ah, 09
+    mov dx, offset linefeed
+    int 21h
+
+    ;values[1] = v1
+    mov ax, v1
+    CALL PRINT
+    mov [v+2] , ax
+    ;new line
+    mov ah, 09
+    mov dx, offset linefeed
+    int 21h
 
     ret
+decrypt ENDP
 
-split ENDP
+PRINT PROC		
+	
+	;initialize count
+	mov cx,0
+	mov dx,0
+	label1:
+		; if ax is zero
+		cmp ax,0
+		je print1	
+		
+		;initialize bx to 10
+		mov bx,10	
+		
+		; extract the last digit
+		div bx				
+		
+		;push it in the stack
+		push dx			
+		
+		;increment the count
+		inc cx			
+		
+		;set dx to 0
+		xor dx,dx
+		jmp label1
+	print1:
+		;check if count
+		;is greater than zero
+		cmp cx,0
+		je exit
+		
+		;pop the top of stack
+		pop dx
+		
+		;add 48 so that it
+		;represents the ASCII
+		;value of digits
+		add dx,48
+		
+		;interrupt to print a
+		;character
+		mov ah,02h
+		int 21h
+		
+		;decrease the count
+		dec cx
+		
+		jmp print1
+exit:
+ret
+PRINT ENDP
+END main
+.EXIT
+END
